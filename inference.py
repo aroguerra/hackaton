a9ae0f87d4d11659a1ce9117a84f37ce2b986f21
Abraham Guerra <abraham.rguerra@gmail.com>
2024-02-07 20:10:29 +0200

    commit

---
+++ inference.py
@@ -0,0 +1,176 @@
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+def load_model(model_path):
+    """
+    Function to load the predictive model with pickle
+    :param model_path: path to model pkl
+    :type model_path: str
+    :return: pkl model
+    """
+    with open(model_path, 'rb') as model_file:
+        loaded_model = pickle.load(model_file)
+    return loaded_model
+
+
+def perform_preprocess(df):
+    practice_sport_mapping = {'never': 0, 'sometimes': 1, 'regularly': 2}
+    df['PracticeSport_encoded'] = df['PracticeSport'].map(practice_sport_mapping)
+
+    # Assuming an ordinal mapping for 'WklyStudyHours'
+    wkly_study_hours_mapping = {'< 5': 0, '5 - 10': 1, '> 10': 2}
+    df['WklyStudyHours_encoded'] = df['WklyStudyHours'].map(wkly_study_hours_mapping)
+
+    parent_educ_mapping = {
+        'some high school': 1,
+        'high school': 2,
+        'some college': 3,
+        "associate's degree": 4,
+        "bachelor's degree": 5,
+        "master's degree": 6
+    }
+
+    df['ParentEduc_Ordinal'] = df['ParentEduc'].map(parent_educ_mapping)
+
+    df['TestPrep_Binary'] = df['TestPrep'].map({'none': 0, 'completed': 1})
+
+    df['LunchType_Binary'] = df['LunchType'].map({'standard': 1, 'free/reduced': 0})
+
+    df['activities'] = df['activities'].map({'yes': 1, 'no': 0})
+
+    df['TransportMeans'] = df['TransportMeans'].map({'school_bus': 1, 'private': 0})
+
+    parent_status_mapping = {
+        'widowed': 1,
+        'single': 2,
+        'divorced': 3,
+        "married": 4,
+    }
+
+    df['ParentMaritalStatus_encoded'] = df['ParentMaritalStatus'].map(parent_status_mapping)
+
+    df = df.drop(columns=['WklyStudyHours', 'PracticeSport', 'ParentEduc', 'LunchType', 'TestPrep', 'ParentMaritalStatus'])
+    return df
+
+
+def perform_inference(model, X_test_path, saved_predictions_path):
+    """
+    Function to validate if the model saved and loaded on inference has the same predictions as
+    the model in train.py
+    :param model: model pkl
+    :type model: pkl
+    :param X_test_path: path to test set
+    :type X_test_path: str
+    :param saved_predictions_path: path to predictions csv of the model
+    :type saved_predictions_path: str
+    """
+    X_test = pd.read_csv(X_test_path)
+    y_predictions_inference = model.predict(X_test)
+    saved_predictions = np.loadtxt(saved_predictions_path, delimiter=',', skiprows=1)
+
+    if np.array_equal(y_predictions_inference, saved_predictions):
+        print("Predictions match.")
+    else:
+        print("Predictions do not match.")
+
+
+@app.route('/predict_grades', methods=['GET'])
+def predict_grade():
+    response = {
+        'IsFirstChild': float(request.args.get('IsFirstChild')),
+        'activities': float(request.args.get('activities')),
+        'freetime': float(request.args.get('freetime')),
+        'NrSiblings': float(request.args.get('NrSiblings')),
+        'PracticeSport': float(request.args.get('PracticeSport')),
+        'WklyStudyHours': float(request.args.get('WklyStudyHours')),
+        'ParentEduc': float(request.args.get('ParentEduc')),
+        'TestPrep': float(request.args.get('TestPrep')),
+        'LunchType': float(request.args.get('LunchType')),
+        'TransportMeans': float(request.args.get('TransportMeans')),
+        'ParentMaritalStatus': float(request.args.get('ParentMaritalStatus')),
+    }
+
+    # mock_response = {
+    #     'internet': 'yes',
+    #     'IsFirstChild': 1,
+    #     'activities': 'yes',
+    #     'schoolsup': 'no',
+    #     'freetime': 3,
+    #     'NrSiblings': 2,
+    #     'PracticeSport': 'regularly',
+    #     'WklyStudyHours': '5 - 10',
+    #     'ParentEduc': "bachelor's degree",
+    #     'TestPrep': 'completed',
+    #     'LunchType': 'standard',
+    #     'TransportMeans': 'private',
+    #     'ParentMaritalStatus': 'married',
+    # }
+    # print(response)
+    # # Creating a DataFrame from the response dictionary
+    df2 = pd.DataFrame([response])
+    # print(df2)
+
+    # is_first_child = float(request.args.get('IsFirstChild'))
+    # activities = float(request.args.get('activities'))
+    # freetime = float(request.args.get('freetime'))
+    # nr_siblings = float(request.args.get('NrSiblings'))
+    # practice_sport_encoded = float(request.args.get('PracticeSport'))
+    # wkly_study_hours_encoded = float(request.args.get('WklyStudyHours_'))
+    # parent_educ_ordinal = float(request.args.get('ParentEduc'))
+    # test_prep_binary = float(request.args.get('TestPrep'))
+    # lunch_type_binary = float(request.args.get('LunchType'))
+    # transport_means_private = float(request.args.get('TransportMeans'))
+    # parent_marital_status_divorced = float(request.args.get('ParentMaritalStatus'))
+
+    #input_data = [is_first_child, activities, freetime, nr_siblings, practice_sport_encoded,
+    #            wkly_study_hours_encoded, parent_educ_ordinal, test_prep_binary, lunch_type_binary,
+    #               transport_means_private, parent_marital_status_divorced]
+
+    prepro_data = perform_preprocess(df2)
+    prepro_data_list = prepro_data.values.tolist()
+
+    # print(df2.value_counts())
+    # print(df2)
+    # print(prepro_data_list)
+
+    prediction_math = model_math.predict(prepro_data_list)[0]
+    prediction_reading = model_reading.predict(prepro_data_list)[0]
+    prediction_writing = model_writing.predict(prepro_data_list)[0]
+    # return
+    return [prediction_math, prediction_reading, prediction_writing]
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'heyyyy'
+
+
+if __name__ == "__main__":
+    #predict_grade()
+    model_path_math = 'studmodel_math.pkl'
+    model_path_reading = 'studmodel_reading.pkl'
+    model_path_writing = 'studmodel_writing.pkl'
+    X_test_path = 'X_test.csv'
+
+    model_math = load_model(model_path_math)
+    model_reading = load_model(model_path_reading)
+    model_writing = load_model(model_path_writing)
+
+    saved_predictions_path = 'preds_math.csv'
+    saved_predictions_path = 'preds_read.csv'
+    saved_predictions_path = 'preds_write.csv'
+
+    perform_inference(model_math, X_test_path, saved_predictions_path)
+    perform_inference(model_reading, X_test_path, saved_predictions_path)
+    perform_inference(model_writing, X_test_path, saved_predictions_path)
+
+
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+# testing example
+# http://localhost:5001/predict_churn?is_male=1&num_inters=0&late_on_payment=1&age=1&years_in_contract=3
---
+++ train.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pickle
+from inference import perform_preprocess
+from sklearn.ensemble import AdaBoostRegressor
+
+if __name__ == "__main__":
+    df = pd.read_csv('combined_shuffled_data.csv')
+    TARGET = ['MathScore', 'ReadingScore', 'WritingScore']
+    FEATS = list(set(df.columns) - set(TARGET))
+
+    X_train, X_test, y_train, y_test = train_test_split(df[FEATS], df[TARGET],
+                                                        test_size=0.2,
+                                                        random_state=42)
+
+    X_train_enc = perform_preprocess(X_train)
+    X_test_enc = perform_preprocess(X_test)
+
+    y_train_MathScore = y_train['MathScore']
+    y_train_ReadingScore = y_train['ReadingScore']
+    y_train_WritingScore = y_train['WritingScore']
+
+    y_test_MathScore = y_test['MathScore']
+    y_test_ReadingScore = y_test['ReadingScore']
+    y_test_WritingScore = y_test['WritingScore']
+
+    regression_model_ada_math = AdaBoostRegressor(n_estimators=100)
+    regression_model_ada_math.fit(X_train_enc, y_train_MathScore)
+    y_pred_ada_math = regression_model_ada_math.predict(X_test_enc)
+
+    regression_model_ada_read = AdaBoostRegressor(n_estimators=100)
+    regression_model_ada_read.fit(X_train_enc, y_train_ReadingScore)
+    y_pred_ada_read = regression_model_ada_read.predict(X_test_enc)
+
+    regression_model_ada_write = AdaBoostRegressor(n_estimators=100)
+    regression_model_ada_write.fit(X_train_enc, y_train_WritingScore)
+    y_pred_ada_write = regression_model_ada_write.predict(X_test_enc)
+
+    X_test.to_csv('X_test.csv', index=False)
+
+    np.savetxt('preds_math.csv', y_pred_ada_math, delimiter=',', header='prediction', comments='')
+    np.savetxt('preds_read.csv', y_pred_ada_read, delimiter=',', header='prediction', comments='')
+    np.savetxt('preds_write.csv', y_pred_ada_write, delimiter=',', header='prediction', comments='')
+
+    with open('studmodel_math.pkl', 'wb') as model_file:
+        pickle.dump(regression_model_ada_math, model_file)
+
+    with open('studmodel_reading.pkl', 'wb') as model_file:
+        pickle.dump(regression_model_ada_read, model_file)
+
+    with open('studmodel_writing.pkl', 'wb') as model_file:
+        pickle.dump(regression_model_ada_write, model_file)
+
+    print("Model saved successfully.")
